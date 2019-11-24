33be46848266b93e44219babf213ee1df895a798
Housni BOUDAOUD <h.boudaoud@hotmail.fr>
2019-11-24 23:37:05 +0100

    Modification du app.component.html et .scss Ajout de components (et des condiion pour leurs affichage):  - post-add : pour l'ajout d'un nouveau post  - post-list : pour afficher la list des posts  - post- list-item: pour afficher un item de la list des posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'application-type-blog';
+  private postList=[];
+  name_component=(this.postList.length>0)?'list':'add';
+
+  change_component(component: string) {
+    this.name_component =  component
+  }
+
+  onClose() {
+    this.name_component ='list'
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+import { PostListItemComponent } from './post-list-item/post-list-item.component';
+import { PostAddComponent } from './post-add/post-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostListItemComponent,
+    PostAddComponent
   ],
   imports: [
     BrowserModule
