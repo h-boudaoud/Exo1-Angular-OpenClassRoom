46362536766f8177c7f67eac5b261d35fc208c85
Housni BOUDAOUD <h.boudaoud@hotmail.fr>
2019-11-25 10:28:33 +0100

    Dernieres modifications de contenues des pages et de css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'application-type-blog';
+  private oldPostList =[
+    {
+      title: 'post n°1',
+      content: 'ceci est le premier post',
+      loveIts: 3,
+      created_at: new Date('2019-11-20 11:05:17')
+    },
+    {
+      title: 'un autre post',
+      content: 'hé oui\nUn nouveau post',
+      loveIts: 0,
+      created_at: new Date('2019-11-21 15:05:17')
+    },
+    {
+      title: 'post numéro 3',
+      content: 'le dernier post inutile\nTestant ce code',
+      loveIts: -3,
+      created_at: new Date('2019-11-22 11:05:17')
+    },
+
+]
+  private pageTitle = {
+    'add': 'ajouter un nouveau post'
+    ,'list': 'la list des posts'
+  }
   private postList=[];
-  name_component=(this.postList.length>0)?'list':'add';
+  private name_component: string;
+  constructor() {
+    this.postList = this.oldPostList
+    this.name_component=(this.postList.length>0)?'list':'add';
+  }
 
   change_component(component: string) {
     this.name_component =  component
@@ -17,4 +46,5 @@ export class AppComponent {
   onClose() {
     this.name_component ='list'
   }
+
 }
